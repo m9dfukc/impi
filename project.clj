86539972c585c598a9309cb14bbb5aa8c0fabd25
m9dfukc <andreas@invertednothing.com>
2018-05-22 13:05:11 +0200

    bumped dependencies to latest versions

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject impi "0.0.10"
+(defproject impi "0.0.11"
   :description "ClojureScript library for using Pixi.js through immutable data"
   :url "https://github.com/weavejester/impi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [weavejester/pixi "4.2.3-0"]]
-  :plugins [[lein-cljsbuild "1.1.3"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [cljsjs/pixi "4.7.0-0"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild
   {:builds
    {:main
